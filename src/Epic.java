be8e4d428d208f47d1eb39cb7e101a4a39aa691c
Dudakova-a <dunas1@yandex.ru>
2025-03-24 15:35:34 +0800

    Add files via upload

---
+++ src/Epic.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
+// Класса для эпиков, наследуем от model.Task
+public class Epic extends Task {
+    private List<Integer> subtaskIds; // Список ids для подзадач входящих в эпик
+
+    // Конструктор для создания эпика
+    public Epic(int id, String name, String description) {
+        super(id, name, description, Status.NEW); // Эпик всегда создается со статусом NEW
+        this.subtaskIds = new ArrayList<>();// Инициализируем список подзадач
+    }
+
+    // Геттер для списка идентификаторов подзадач
+    public List<Integer> getSubtaskIds() {
+        return subtaskIds;
+    }
+
+    // Метод для добавления индентификатора подзадачи в эпик
+    public void addSubtaskId(int subtaskID) {
+        subtaskIds.add(subtaskID);
+    }
+
+    // Метод для удаления индентификатора подзадачи из эпика
+    public void removeSubtaskId(int subtaskID) {
+        subtaskIds.remove((Integer) subtaskID); // Удаляем по значению
+    }
+}
---
+++ src/Status.java
@@ -0,0 +1,8 @@
+public enum Status {
+    NEW,
+    IN_PROGRESS,
+    DONE;
+
+    private Status() {
+    }
+}
